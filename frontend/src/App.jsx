bc616ed97280e36fa5e8ee53c7a83646dccec2ed
Calle <ca.karlsson@hotmail.com>
2025-04-18 17:19:10 +0200

    Adding needed states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,25 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const handleGuessSubmit = async () => {};
+
 function App() {
+  const [guess, setGuess] = useState("");
+  const [guesses, setGuesses] = useState([]);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [playerName, setPlayerName] = useState("");
+  const [wordLength, setWordLength] = useState(5);
   const [allowReapeatingLetters, setAllowReapeatingLetters] = useState(false);
 
   return (
     <main className="App">
       <div className="container">
         <h1 className="App__Header">Fake wordle</h1>
+        <p>Guess the word!</p>
+
+        <form onSubmit={handleGuessSubmit}></form>
+
         <label>
           Select word length:
           <select>
@@ -25,10 +37,14 @@ function App() {
         </label>
         <label>
           Allow repeating letters:
-          <input type="checkbox" checked={allowReapeatingLetters} onChange={'placeholder'} />
-          <button className="play__button" >Play</button>
+          <input
+            type="checkbox"
+            checked={allowReapeatingLetters}
+            onChange={"placeholder"}
+          />
         </label>
       </div>
+      <button className="play__button">Play</button>
     </main>
   );
 }
