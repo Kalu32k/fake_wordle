00d7d6bfebcc553131d304704db2392b9f8a9d7d
Calle <ca.karlsson@hotmail.com>
2025-04-18 17:52:56 +0200

    Building the api routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
-import express from 'express';
+import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 5080;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", async (req, res) => {
+    const htmlText = await fs.promises.readFile("frontend/dist/index.html");
+    res.send(htmlText.toString());
+  });
+
+  app.get("/about", async (req, res) => {
+    const htmlText = await fs.promises.readFile("frontend/dist/about.html");
+    res.send(htmlText.toString());
+  });
+
+app.get('/api/random', async (req, res) => {
 });
 
-app.listen(port, () => {    
-    console.log(`Server is running at http://localhost:${port}`);
+app.get('/api/guess'), async (req, res) => {
+    
+}
+
+app.use("/assets", express.static("frontend/dist/assets"));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
